a16fa2f3ff11e96f9559dc3909bb5f0c9f771014
Soham Mondal <159754444+SantuProf@users.noreply.github.com>
2025-08-13 14:26:27 +0530

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/soham/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/soham/Documents/app development/mind_it" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/soham/flutter"
+  "PROJECT_DIR=/home/soham/Documents/app development/mind_it"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/soham/Documents/app development/mind_it/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/soham/Documents/app development/mind_it/lib/main.dart"
+)
